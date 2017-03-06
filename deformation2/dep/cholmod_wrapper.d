53c730455f71a3f4f404052ff009aaff02070aed
Hou Peihong <kjustdoitno1@gmail.com>
2017-03-06 15:31:49 +0800

    deform-transfer

---
+++ deformation2/dep/cholmod_wrapper.d
@@ -0,0 +1,72 @@
+cholmod_wrapper.o dep/cholmod_wrapper.d : ../common/cholmod_wrapper.c c:\mingw\include\stdlib.h \
+ c:\mingw\include\_mingw.h c:\mingw\include\msvcrtver.h \
+ c:\mingw\include\w32api.h c:\mingw\include\sdkddkver.h \
+ c:\mingw\lib\gcc\mingw32\5.3.0\include\stddef.h c:\mingw\include\stdio.h \
+ c:\mingw\include\sys/types.h \
+ c:\mingw\lib\gcc\mingw32\5.3.0\include\stdarg.h \
+ ../common/cholmod_wrapper.h c:\mingw\include\windows.h \
+ c:\mingw\include\windef.h c:\mingw\include\winnt.h \
+ c:\mingw\include\winerror.h c:\mingw\include\string.h \
+ c:\mingw\include\strings.h c:\mingw\include\wchar.h \
+ c:\mingw\include\basetsd.h c:\mingw\include\pshpack4.h \
+ c:\mingw\include\poppack.h c:\mingw\include\wincon.h \
+ c:\mingw\include\winbase.h c:\mingw\include\wingdi.h \
+ c:\mingw\include\winuser.h c:\mingw\include\winnls.h \
+ c:\mingw\include\winver.h c:\mingw\include\winnetwk.h \
+ c:\mingw\include\winreg.h c:\mingw\include\winsvc.h \
+ c:\mingw\include\cderr.h c:\mingw\include\dde.h c:\mingw\include\ddeml.h \
+ c:\mingw\include\dlgs.h c:\mingw\include\imm.h \
+ c:\mingw\include\lzexpand.h c:\mingw\include\mmsystem.h \
+ c:\mingw\include\nb30.h c:\mingw\include\rpc.h c:\mingw\include\rpcdce.h \
+ c:\mingw\include\basetyps.h c:\mingw\include\rpcdcep.h \
+ c:\mingw\include\rpcnsi.h c:\mingw\include\rpcnterr.h \
+ c:\mingw\include\shellapi.h c:\mingw\include\pshpack2.h \
+ c:\mingw\include\winperf.h c:\mingw\include\commdlg.h \
+ c:\mingw\include\unknwn.h c:\mingw\include\objfwd.h \
+ c:\mingw\include\wtypes.h c:\mingw\include\rpcndr.h \
+ c:\mingw\include\rpcnsip.h c:\mingw\include\prsht.h \
+ c:\mingw\include\winspool.h c:\mingw\include\winsock2.h \
+ c:\mingw\include\ole2.h c:\mingw\include\objbase.h \
+ c:\mingw\include\objidl.h c:\mingw\include\cguid.h \
+ c:\mingw\include\olectlid.h c:\mingw\include\oleauto.h \
+ c:\mingw\include\oaidl.h c:\mingw\include\oleidl.h \
+ ../external/include/cholmod.h ../external/include/cholmod_io64.h \
+ ../external/include/UFconfig.h c:\mingw\include\limits.h \
+ ../external/include/cholmod_config.h ../external/include/cholmod_core.h \
+ ../external/include/cholmod_check.h \
+ ../external/include/cholmod_cholesky.h \
+ ../external/include/cholmod_partition.h \
+ ../external/include/cholmod_supernodal.h \
+ ../external/include/cholmod_matrixops.h \
+ ../external/include/cholmod_modify.h ../external/include/umfpack.h \
+ ../external/include/umfpack_symbolic.h \
+ ../external/include/umfpack_numeric.h \
+ ../external/include/umfpack_solve.h \
+ ../external/include/umfpack_free_symbolic.h \
+ ../external/include/umfpack_free_numeric.h \
+ ../external/include/umfpack_defaults.h \
+ ../external/include/umfpack_qsymbolic.h \
+ ../external/include/umfpack_wsolve.h \
+ ../external/include/umfpack_triplet_to_col.h \
+ ../external/include/umfpack_col_to_triplet.h \
+ ../external/include/umfpack_transpose.h \
+ ../external/include/umfpack_scale.h \
+ ../external/include/umfpack_get_lunz.h \
+ ../external/include/umfpack_get_numeric.h \
+ ../external/include/umfpack_get_symbolic.h \
+ ../external/include/umfpack_save_numeric.h \
+ ../external/include/umfpack_load_numeric.h \
+ ../external/include/umfpack_save_symbolic.h \
+ ../external/include/umfpack_load_symbolic.h \
+ ../external/include/umfpack_get_determinant.h \
+ ../external/include/umfpack_report_status.h \
+ ../external/include/umfpack_report_info.h \
+ ../external/include/umfpack_report_control.h \
+ ../external/include/umfpack_report_matrix.h \
+ ../external/include/umfpack_report_triplet.h \
+ ../external/include/umfpack_report_vector.h \
+ ../external/include/umfpack_report_symbolic.h \
+ ../external/include/umfpack_report_numeric.h \
+ ../external/include/umfpack_report_perm.h \
+ ../external/include/umfpack_timer.h ../external/include/umfpack_tictoc.h \
+ ../external/include/amd.h ../external/include/umfpack_global.h
---
+++ deformation2/dep/gl_util.d
@@ -0,0 +1,43 @@
+gl_util.o dep/gl_util.d : ../gl_util/gl_util.c ../gl_util/gl_util.h \
+ c:\mingw\include\windows.h \
+ c:\mingw\lib\gcc\mingw32\5.3.0\include\stdarg.h \
+ c:\mingw\include\windef.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdkddkver.h c:\mingw\include\winnt.h \
+ c:\mingw\include\winerror.h c:\mingw\include\string.h \
+ c:\mingw\include\_mingw.h c:\mingw\include\msvcrtver.h \
+ c:\mingw\lib\gcc\mingw32\5.3.0\include\stddef.h \
+ c:\mingw\include\strings.h c:\mingw\include\wchar.h \
+ c:\mingw\include\basetsd.h c:\mingw\include\pshpack4.h \
+ c:\mingw\include\poppack.h c:\mingw\include\wincon.h \
+ c:\mingw\include\winbase.h c:\mingw\include\wingdi.h \
+ c:\mingw\include\winuser.h c:\mingw\include\winnls.h \
+ c:\mingw\include\winver.h c:\mingw\include\winnetwk.h \
+ c:\mingw\include\winreg.h c:\mingw\include\winsvc.h \
+ c:\mingw\include\cderr.h c:\mingw\include\dde.h c:\mingw\include\ddeml.h \
+ c:\mingw\include\dlgs.h c:\mingw\include\imm.h \
+ c:\mingw\include\lzexpand.h c:\mingw\include\mmsystem.h \
+ c:\mingw\include\nb30.h c:\mingw\include\rpc.h c:\mingw\include\rpcdce.h \
+ c:\mingw\include\basetyps.h c:\mingw\include\rpcdcep.h \
+ c:\mingw\include\rpcnsi.h c:\mingw\include\rpcnterr.h \
+ c:\mingw\include\shellapi.h c:\mingw\include\pshpack2.h \
+ c:\mingw\include\winperf.h c:\mingw\include\commdlg.h \
+ c:\mingw\include\unknwn.h c:\mingw\include\objfwd.h \
+ c:\mingw\include\wtypes.h c:\mingw\include\rpcndr.h \
+ c:\mingw\include\rpcnsip.h c:\mingw\include\prsht.h \
+ c:\mingw\include\winspool.h c:\mingw\include\winsock2.h \
+ c:\mingw\include\ole2.h c:\mingw\include\objbase.h \
+ c:\mingw\include\stdlib.h c:\mingw\include\objidl.h \
+ c:\mingw\include\cguid.h c:\mingw\include\olectlid.h \
+ c:\mingw\include\oleauto.h c:\mingw\include\oaidl.h \
+ c:\mingw\include\oleidl.h c:\mingw\include\gl\glut.h \
+ c:\mingw\include\gl\gl.h c:\mingw\include\gl\glu.h \
+ ../common/mesh_model.h ../common/dt_type.h ../common/cholmod_wrapper.h \
+ ../external/include/cholmod.h ../external/include/cholmod_io64.h \
+ ../external/include/UFconfig.h c:\mingw\include\limits.h \
+ ../external/include/cholmod_config.h ../external/include/cholmod_core.h \
+ ../external/include/cholmod_check.h c:\mingw\include\stdio.h \
+ c:\mingw\include\sys/types.h ../external/include/cholmod_cholesky.h \
+ ../external/include/cholmod_partition.h \
+ ../external/include/cholmod_supernodal.h \
+ ../external/include/cholmod_matrixops.h \
+ ../external/include/cholmod_modify.h
